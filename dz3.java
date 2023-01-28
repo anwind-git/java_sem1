6c1e95a88dcec2b2cf517f81b562948a3078d122
anwind-git <work-git@yandex.ru>
2023-01-28 15:16:08 +0300

    Добавлено логирование к калькулятору

--- dz3.java
+++ dz3.java
@@ -1,8 +1,12 @@
 // Реализовать простой калькулятор
+// Дополнение семинар 2: К калькулятору из предыдущего дз добавить логирование
 
 import java.util.Scanner;
+import java.io.IOException;
+import java.util.logging.*;
 
 public class dz3 {
+
 
     static int iScannerInt() {
         Scanner iScanner = new Scanner(System.in);
@@ -16,7 +20,13 @@ public class dz3 {
             return operations;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SecurityException, IOException {
+        Logger logger = Logger.getLogger(dz1.class.getName());
+        FileHandler fh = new FileHandler("log_dz3.txt");
+        logger.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+
         System.out.printf("Введите первое число: ");
         int num1 = iScannerInt();
         System.out.printf("Введите оператор: ");
@@ -27,18 +37,22 @@ public class dz3 {
         switch (operations) {
             case "+":
                 result = num1+num2;
+                logger.info("Выполнение операции: " + num1 + "+" + num2 + "=" + result);
                 break;
             case "-":
                 result = num1-num2;
+                logger.info("Выполнение операции: " + num1 + "+" + num2 + "-" + result);
                 break;
             case "*":
                 result = num1*num2;
+                logger.info("Выполнение операции: " + num1 + "*" + num2 + "=" + result);
                 break;
             case "/":
                 result = num1/num2;
+                logger.info("Выполнение операции: " + num1 + "/" + num2 + "=" + result);
                 break;
             default:
-                System.out.println("Неверная операция. Повторите ввод.");
+                logger.info("Произошла ошибка ввода");
         }
         System.out.printf("Результат: %s %s %s = %s", num1, operations, num2, result);
     }
